f7cf2b7534027642288564de26222e3655584a43
MatrixMTX <byeonseho@gmail.com>
2024-07-24 22:34:26 +0900

    Update KurzHub.lua

--- KurzHub.lua
+++ KurzHub.lua
@@ -1,11 +1,30 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Kurz Hub", HidePremium = false, IntroText = "Kurz Hub Loader", SaveConfig = true, ConfigFolder = "KurzHub"})
-local Players = game:GetService("Players"):GetChildren()
+local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+local Camera = workspace.CurrentCamera
 
 -- Values
+local Typing = false
 _G.ESP = false
 _G.Color = Color3.fromHSV(0,0,1)
+
+_G.SendNotifications = true   -- If set to true then the script would notify you frequently on any changes applied and when loaded / errored. (If a game can detect this, it is recommended to set it to false)
+_G.DefaultSettings = false   -- If set to true then the ESP script would run with default settings regardless of any changes you made.
+    
+_G.TeamCheck = false   -- If set to true then the script would create ESP only for the enemy team members.
+    
+_G.ESPVisible = false   -- If set to true then the ESP will be visible and vice versa.
+_G.TextColor = Color3.fromRGB(255, 80, 10)   -- The color that the boxes would appear as.
+_G.TextSize = 14   -- The size of the text.
+_G.Center = true   -- If set to true then the script would be located at the center of the label.
+_G.Outline = true   -- If set to true then the text would have an outline.
+_G.OutlineColor = Color3.fromRGB(0, 0, 0)   -- The outline color of the text.
+_G.TextTransparency = 0.7   -- The transparency of the text.
+_G.TextFont = Drawing.Fonts.UI   -- The font of the text. (UI, System, Plex, Monospace) 
+
 _G.WS = 16
 _G.JH = 50
 _G.isJumping = false
@@ -15,8 +34,99 @@ function ESP()
 	if _G.ESP then
         local highlight = Instance.new("Highlight")
         highlight.Name = "Highlight"
+
+        for _, v in next, Players:GetPlayers() do
+            if v.Name ~= Players.LocalPlayer.Name then
+                local ESP = Drawing.new("Text")
+    
+                RunService.RenderStepped:Connect(function()
+                    if workspace:FindFirstChild(v.Name) ~= nil and workspace[v.Name]:FindFirstChild("HumanoidRootPart") ~= nil then
+                        local Vector, OnScreen = Camera:WorldToViewportPoint(workspace[v.Name]:WaitForChild("Head", math.huge).Position)
+    
+                        ESP.Size = _G.TextSize
+                        ESP.Center = _G.Center
+                        ESP.Outline = _G.Outline
+                        ESP.OutlineColor = _G.OutlineColor
+                        ESP.Color = _G.TextColor
+                        ESP.Transparency = _G.TextTransparency
+                        ESP.Font = _G.TextFont
+    
+                        if OnScreen == true then
+                            local Part1 = workspace:WaitForChild(v.Name, math.huge):WaitForChild("HumanoidRootPart", math.huge).Position
+                            local Part2 = workspace:WaitForChild(Players.LocalPlayer.Name, math.huge):WaitForChild("HumanoidRootPart", math.huge).Position or 0
+                            local Dist = (Part1 - Part2).Magnitude
+                            ESP.Position = Vector2.new(Vector.X, Vector.Y - 25)
+                            ESP.Text = ("("..tostring(math.floor(tonumber(Dist)))..") "..v.Name.." ["..workspace[v.Name].Humanoid.Health.."]")
+                            if _G.TeamCheck == true then 
+                                if Players.LocalPlayer.Team ~= v.Team then
+                                    ESP.Visible = _G.ESPVisible
+                                else
+                                    ESP.Visible = false
+                                end
+                            else
+                                ESP.Visible = _G.ESPVisible
+                            end
+                        else
+                            ESP.Visible = false
+                        end
+                    else
+                        ESP.Visible = false
+                    end
+                end)
+    
+                Players.PlayerRemoving:Connect(function()
+                    ESP.Visible = false
+                end)
+            end
+        end
+    
+        Players.PlayerAdded:Connect(function(Player)
+            Player.CharacterAdded:Connect(function(v)
+                if v.Name ~= Players.LocalPlayer.Name then 
+                    local ESP = Drawing.new("Text")
+    
+                    RunService.RenderStepped:Connect(function()
+                        if workspace:FindFirstChild(v.Name) ~= nil and workspace[v.Name]:FindFirstChild("HumanoidRootPart") ~= nil then
+                            local Vector, OnScreen = Camera:WorldToViewportPoint(workspace[v.Name]:WaitForChild("Head", math.huge).Position)
+    
+                            ESP.Size = _G.TextSize
+                            ESP.Center = _G.Center
+                            ESP.Outline = _G.Outline
+                            ESP.OutlineColor = _G.OutlineColor
+                            ESP.Color = _G.TextColor
+                            ESP.Transparency = _G.TextTransparency
+    
+                            if OnScreen == true then
+                                local Part1 = workspace:WaitForChild(v.Name, math.huge):WaitForChild("HumanoidRootPart", math.huge).Position
+                                local Part2 = workspace:WaitForChild(Players.LocalPlayer.Name, math.huge):WaitForChild("HumanoidRootPart", math.huge).Position or 0
+                                local Dist = (Part1 - Part2).Magnitude
+                                ESP.Position = Vector2.new(Vector.X, Vector.Y - 25)
+                                ESP.Text = ("("..tostring(math.floor(tonumber(Dist)))..") "..v.Name.." ["..workspace[v.Name].Humanoid.Health.."]")
+                                if _G.TeamCheck == true then 
+                                    if Players.LocalPlayer.Team ~= Player.Team then
+                                        ESP.Visible = _G.ESPVisible
+                                    else
+                                        ESP.Visible = false
+                                    end
+                                else
+                                    ESP.Visible = _G.ESPVisible
+                                end
+                            else
+                                ESP.Visible = false
+                            end
+                        else
+                            ESP.Visible = false
+                        end
+                    end)
+    
+                    Players.PlayerRemoving:Connect(function()
+                        ESP.Visible = false
+                    end)
+                end
+            end)
+        end)
 		-- 활성화 로직
-		for i, v in pairs(Players) do
+		for i, v in pairs(Players:GetChildren()) do
 			repeat wait() until v.Character
 			if not v.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") and _G.ESP then
 				local highlightClone = highlight:Clone()
@@ -30,26 +140,85 @@ function ESP()
 		end
 
 		game.Players.PlayerAdded:Connect(function(player)
-			repeat wait() until player.Character
-			if not player.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") and _G.ESP then
-				local highlightClone = highlight:Clone()
-				highlightClone.Adornee = player.Character
-				highlightClone.Parent = player.Character:FindFirstChild("HumanoidRootPart")
-                highlightClone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-				highlightClone.OutlineColor = _G.Color
-				highlightClone.FillTransparency = 1
-				highlightClone.Name = "Highlight"
-			end
+			Player.CharacterAdded:Connect(function(v)
+                if not player.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") and _G.ESP then
+                    local highlightClone = highlight:Clone()
+                    highlightClone.Adornee = player.Character
+                    highlightClone.Parent = player.Character:FindFirstChild("HumanoidRootPart")
+                    highlightClone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+                    highlightClone.OutlineColor = _G.Color
+                    highlightClone.FillTransparency = 1
+                    highlightClone.Name = "Highlight"
+                end
+                if v.Name ~= Players.LocalPlayer.Name then 
+                    local ESP = Drawing.new("Text")
+
+                    RunService.RenderStepped:Connect(function()
+                        if workspace:FindFirstChild(v.Name) ~= nil and workspace[v.Name]:FindFirstChild("HumanoidRootPart") ~= nil then
+                            local Vector, OnScreen = Camera:WorldToViewportPoint(workspace[v.Name]:WaitForChild("Head", math.huge).Position)
+
+                            ESP.Size = _G.TextSize
+                            ESP.Center = _G.Center
+                            ESP.Outline = _G.Outline
+                            ESP.OutlineColor = _G.OutlineColor
+                            ESP.Color = _G.TextColor
+                            ESP.Transparency = _G.TextTransparency
+
+                            if OnScreen == true then
+                                local Part1 = workspace:WaitForChild(v.Name, math.huge):WaitForChild("HumanoidRootPart", math.huge).Position
+                                local Part2 = workspace:WaitForChild(Players.LocalPlayer.Name, math.huge):WaitForChild("HumanoidRootPart", math.huge).Position or 0
+                                local Dist = (Part1 - Part2).Magnitude
+                                ESP.Position = Vector2.new(Vector.X, Vector.Y - 25)
+                                ESP.Text = ("("..tostring(math.floor(tonumber(Dist)))..") "..v.Name.." ["..workspace[v.Name].Humanoid.Health.."]")
+                                if _G.TeamCheck == true then 
+                                    if Players.LocalPlayer.Team ~= Player.Team then
+                                        ESP.Visible = _G.ESPVisible
+                                    else
+                                        ESP.Visible = false
+                                    end
+                                else
+                                    ESP.Visible = _G.ESPVisible
+                                end
+                            else
+                                ESP.Visible = false
+                            end
+                        else
+                            ESP.Visible = false
+                        end
+                    end)
+
+                    Players.PlayerRemoving:Connect(function(playerRemoved)
+                        ESP.Visible = false
+                        if playerRemoved.Character and playerRemoved.Character:FindFirstChild("HumanoidRootPart") and playerRemoved.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") and _G.ESP then
+                            playerRemoved.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight"):Destroy()
+                        end
+                    end)
+                end
+            end)
 		end)
 
-		game.Players.PlayerRemoving:Connect(function(playerRemoved)
-			if playerRemoved.Character and playerRemoved.Character:FindFirstChild("HumanoidRootPart") and playerRemoved.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") and _G.ESP then
-				playerRemoved.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight"):Destroy()
-			end
-		end)
+        if _G.DefaultSettings == true then
+            _G.TeamCheck = false
+            _G.ESPVisible = true
+            _G.TextColor = Color3.fromRGB(40, 90, 255)
+            _G.TextSize = 14
+            _G.Center = true
+            _G.Outline = false
+            _G.OutlineColor = Color3.fromRGB(0, 0, 0)
+            _G.DisableKey = Enum.KeyCode.Q
+            _G.TextTransparency = 0.75
+        end
+        
+        UserInputService.TextBoxFocused:Connect(function()
+            Typing = true
+        end)
+        
+        UserInputService.TextBoxFocusReleased:Connect(function()
+            Typing = false
+        end)
 
 		RunService.Heartbeat:Connect(function()
-			for i, v in pairs(Players) do
+			for i, v in pairs(Players:GetChildren()) do
 				repeat wait() until v.Character
 				if not v.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") and _G.ESP then
 					local highlightClone = highlight:Clone()
@@ -64,7 +233,8 @@ function ESP()
 			end
 		end)
 	else
-		for i, v in pairs(Players) do
+        _G.ESPVisible = not _G.ESPVisible
+		for i, v in pairs(Players:GetChildren()) do
 			if v.Character and v.Character:FindFirstChild("HumanoidRootPart") and v.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then
 				v.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight"):Destroy()
 			end
@@ -138,6 +308,7 @@ EspTab:AddToggle({
     Default = false,
 	Callback = function(Value)
       	_G.ESP = Value
+        _G.ESPVisible = not _G.ESPVisible
         ESP()
         print(Value)
   	end    
